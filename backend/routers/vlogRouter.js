e79a614f056fbc7e1e56211c8f9ece0e7ce9e128
m-triple-m <triplem656@gmail.com>
2022-07-29 13:59:32 +0530

    ..

--- backend/routers/vlogRouter.js
+++ backend/routers/vlogRouter.js
@@ -1,48 +1,64 @@
-const Model = require('../models/vlogModel');
-const router = require('express').Router();
+const Model = require("../models/vlogModel")
+const router = require("express").Router()
 
-router.post('/add',(req,res) => {
-    console.log(req.body);
-   new Model(req.body).save()
-   .then((result) => {
-    console.log(result);
-    console.log('data saved');
-    res.json(result);
-   }).catch((err) => {
-    console.log(err);
-    res.json(err); 
-   });
-  });
+router.post("/add", (req, res) => {
+  console.log(req.body)
+  new Model(req.body)
+    .save()
+    .then((result) => {
+      console.log(result)
+      console.log("data saved")
+      res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.json(err)
+    })
+})
 
-  router.get('/getall',(req,res) => {
-    Model.find({})
+router.get("/getall", (req, res) => {
+  Model.find({})
     .then((result) => {
-      console.log(result);
-      res.json(result);
-    }).catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
-  });
+      console.log(result)
+      res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.json(err)
+    })
+})
 
-  router.delete('/delete/:userid',(req,res) =>{
-    Model.findByIdAndDelete(req.params.userid)
+router.delete("/delete/:userid", (req, res) => {
+  Model.findByIdAndDelete(req.params.userid)
     .then((result) => {
-      res.json(result);
-    }).catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
-  });
-  
-  router.put('/update/:userid',(req,res) =>{
-    Model.findByIdAndUpdate(req.params.userid,req.body,{new : true})
+      res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.json(err)
+    })
+})
+
+router.get("/getbyid/:userid", (req, res) => {
+  Model.findById(req.params.userid)
     .then((result) => {
-      res.json(result);
-    }).catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
-  });
-  
-  module.exports = router;
+      res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.json(err)
+    })
+})
+
+router.put("/update/:userid", (req, res) => {
+  Model.findByIdAndUpdate(req.params.userid, req.body, { new: true })
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.json(err)
+    })
+})
+
+module.exports = router
--- frontend/src/components/viewVlog.js
+++ frontend/src/components/viewVlog.js
@@ -1,10 +1,56 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 const ViewVlog = () => {
   const { id } = useParams()
 
-  console.log(id)
+  console.log(id);
+
+  const url = "http://localhost:5000";
+  const [podcastData, setPodcastData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getPodcastById = () => {
+    setLoading(true);
+    fetch(url + "/vlog/getbyid/" + id)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPodcastData(data);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getPodcastById();
+  }, []);
+
+  const displayPodcastData = () => {
+    console.log(loading);
+    if(!loading && podcastData){
+      return <div className="card">
+      <div className="row">
+        <div className="col">
+          <img src={url+'/'+podcastData.thumbnail} alt="" />
+        </div>
+        <div className="col">
+          <div className="card-body">
+            <h5 className="card-title">Title: {podcastData.title}</h5>
+            <p className="card-text">Description:</p>
+            <p className="card-text">File:</p>
+            <p className="card-text">
+              <small className="text-muted">Uploaded By:</small>
+            </p>
+            <video src={url+'/'+podcastData.file} autoPlay controls></video>
+          </div>
+          </div>
+          </div>
+          </div>
+    }
+    }
+
   return <div>ViewVlog</div>
 }
 
